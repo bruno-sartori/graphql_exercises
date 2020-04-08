9c6b467847c652550ee19366dd17c8da7784bc36
Bruno Sartori <bruno.sartori@agilecontent.com>
2020-04-08 08:52:21 -0300

    adiicona 2 projetos

---
+++ gql-with-knex/src/resolvers/Fornecedor.js
@@ -0,0 +1,9 @@
+module.exports = {
+    Query: {
+        users: () => User.find(),
+        user: (_, { id }) => User.findById(id),
+    },
+    Mutation: {
+        createUser: (_, { name, email }) => User.create({ name, email }),
+    }
+}
--- gql-with-knex/src/server.js
+++ gql-with-knex/src/server.js
@@ -3,14 +3,31 @@ const path = require('path');
 const resolvers = require('./resolvers');
 const mongoose = require('mongoose');
 
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: 'localhost',
+        user: 'oton',
+        password: 'zeta@odin@145',
+        database: 'isp_dev'
+    },
+});
+
+const getUsers = async () => knex('fornecedores').select('nomeFantasia');
+
+getUsers().then(res => console.log(res));
+
 mongoose.connect('mongodb://localhost:27017/graphqlnode', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+}); 
 
 const server = new GraphQLServer({
     typeDefs: path.resolve(__dirname, 'schema.graphql'),
     resolvers,
+    context: {
+        db: knex
+    }
 });
 
 server.start();
